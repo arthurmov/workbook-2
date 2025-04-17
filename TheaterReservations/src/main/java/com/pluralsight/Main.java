dd9f3b659870b64ea75ad6b55e15581116263e3e
arthurmov <officialamovsesyan@gmail.com>
2025-04-17 16:45:53 -0400

    Added comments

--- TheaterReservations/src/main/java/com/pluralsight/Main.java
+++ TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -11,9 +11,11 @@ public class Main {
         System.out.println("Welcome to the Theater! To make a reservation, I will need" +
                 " you to fill out this form for ticket reservations.");
 
+        //establish known values
         System.out.println("Please enter your first and last name: ");
         String name = scanner.nextLine().trim();
 
+        //separates first and last name
         int spaceBetweenName = name.indexOf(" ");
         String firstName = name.substring(0, spaceBetweenName);
         String lastName = name.substring(spaceBetweenName + 1);
@@ -21,11 +23,14 @@ public class Main {
         System.out.println("What date will you be coming (MM/dd/yyyy): ");
         String dateInput = scanner.nextLine();
 
+        //formats the date
         DateTimeFormatter dateFormatter;
         dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
         LocalDate date = LocalDate.parse(dateInput, dateFormatter);
 
+
+        //checks if ticket is 1 or more for different print statements
         System.out.println("How many tickets would you like?");
         int tickets = scanner.nextInt();
 
